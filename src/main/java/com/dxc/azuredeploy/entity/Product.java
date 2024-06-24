561398fc30e76fadf05b8b334c2d060c03e74eff
rayudu <rayuduuppu2001@gmail.com>
2024-06-24 13:13:10 +0530

    changes made

--- src/main/java/com/dxc/azuredeploy/entity/Product.java
+++ src/main/java/com/dxc/azuredeploy/entity/Product.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -13,11 +14,12 @@ import javax.persistence.Table;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "PRODUCT_TBL")
+@Table
+
 public class Product {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private int quantity;
