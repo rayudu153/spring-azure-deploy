7a24cc1dce37af6e1b760c1dba9cf78d39bb5dd5
rayudu <rayuduuppu2001@gmail.com>
2024-06-24 14:55:11 +0530

    changes made

---
+++ src/test/java/com/dxc/azuredeploy/SpringBootAzureDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.dxc.azuredeploy;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SpringBootAzureDeploymentApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
